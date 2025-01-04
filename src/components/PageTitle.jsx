9c88f94a5daf02f827c157460ce144edc3cc194e
fatihozoglu <fatih.ozoglu.91@gmail.com>
2025-01-05 01:04:35 +0300

    Fix page title style

--- src/components/PageTitle.jsx
+++ src/components/PageTitle.jsx
@@ -6,7 +6,7 @@ export default function PageTitle({ children }) {
       </h1>
       <div className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 w-24 h-24 bg-blue-50 rounded-full blur-2xl opacity-60" />
       <div className="mt-4 flex justify-center">
-        <div className="w-20 h-1.5 bg-gradient-to-r from-blue-600 to-indigo-600 rounded-full" />
+        <div className="w-32 h-1.5 bg-gradient-to-r from-blue-600 to-indigo-600 rounded-full" />
       </div>
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-32 h-32 bg-indigo-50 rounded-full blur-3xl opacity-40 -z-10" />
     </div>
