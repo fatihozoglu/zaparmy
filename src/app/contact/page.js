6d54111452b7c1302d3f4c95f247a48888622595
fatihozoglu <fatih.ozoglu.91@gmail.com>
2025-01-05 02:33:02 +0300

    Fix contact page address redirection and remove Map

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,6 +1,4 @@
 'use client';
-import { useEffect, useState } from 'react';
-import dynamic from 'next/dynamic';
 import PageTitle from '@/components/PageTitle';
 import ContactForm from '@/components/ContactForm';
 import { Card } from '@radix-ui/themes';
@@ -11,11 +9,6 @@ import {
   EnvelopeClosedIcon, 
   MobileIcon 
 } from '@radix-ui/react-icons';
-
-const Map = dynamic(
-  () => import('@/components/Map'),
-  { ssr: false }
-);
 
 export default function ContactPage() {
   return (
@@ -34,7 +27,7 @@ export default function ContactPage() {
               <div className="space-y-2 text-gray-600">
                 <a
                     className="hover:text-blue-600"
-                    href="https://maps.google.com/?q=40.93612141049471, 29.134720604643068"
+                    href="https://maps.app.goo.gl/PU2LKBLLrKzMF8Nh8"
                     target="_blank"
                     rel="noopener noreferrer">
                     <div className="flex flex-col">
@@ -93,10 +86,16 @@ export default function ContactPage() {
       </div>
 
       {/* Full Width Map */}
-      <Card>
-        <div className="h-[400px] w-full">
-          <Map />
-        </div>
+      <Card style={{ padding: '0', backgroundColor: '#d0d0d0' }}>
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d201.1360873560138!2d29.134542312132552!3d40.935990108536856!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14cac7fa52e2ded9%3A0x2c2ef29261b9747d!2sZAPARMY!5e0!3m2!1str!2str!4v1736032872223!5m2!1str!2str" 
+            width="100%" 
+            height="450" 
+            style={{ border: 0 }} 
+            allowFullScreen="" 
+            loading="lazy" 
+            referrerPolicy="no-referrer-when-downgrade"
+        />
       </Card>
     </main>
   );
